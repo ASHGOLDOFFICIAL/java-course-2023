38440011af49e1e9690b2267ceb2402efa44b1f8
Andrey Shaat <links.goldwater@gmail.com>
2023-10-12 01:09:19 +0500

    Added solution for task 5 from homework 1

---
+++ src/main/java/edu/hw1/Task5.java
@@ -0,0 +1,65 @@
+package edu.hw1;
+
+import edu.Task;
+
+public class Task5 extends Task {
+    private Task5() {
+    }
+
+    private static boolean isPalindrome(String str) {
+        final int strLength = str.length();
+
+        boolean isPalindrone = true;
+        for (int i = 0; i < strLength / 2 + (strLength % 2); i++) {
+            if (str.charAt(i) != str.charAt(strLength - 1 - i)) {
+                isPalindrone = false;
+                break;
+            }
+        }
+
+        return isPalindrone;
+    }
+
+    public static boolean isPalindromeDescendant(int num) {
+        if (num < 10) return false;
+
+        String numString = String.valueOf(num);
+        if (isPalindrome(numString)) return true;
+        if (numString.length() % 2 == 1) return false;
+
+        int localNum = num;
+        int count = 0;
+        int lastDigit = 0;
+        int newNum = 0;
+        int decimalShift = 1;
+
+        while (localNum != 0) {
+//            Берём последнюю цифру числа, затем "выкидываем" её.
+            final int digit = localNum % 10;
+            localNum /= 10;
+
+/*
+            Если итерация нечётная, т.е. каждая вторая (начинаем с 0), то суммируем текущую цифру и прошлую,
+            "приписываем" справа к полученной сумме нули (decimalShift) и добавляем к этому числу предыдущее значение
+            искомого числа. Затем увеличиваем decimalShift в 10 раз, если сумма текущей и прошлой цифр состоит из
+            одной цифры, или в 100 раз, если их сумма состоит из двух цифр.
+
+            Например, потомок числа 130177 будет получен следующим образом:
+             - (7 + 7) * 1 + 0 = 14
+               (т.к. 7+7=14 состоит из 2 цифр, то в следующей нечётной итерации произведём сдвиг на один ноль больше)
+             - (0 + 1) * 100 + 14 = 114
+             - (3 + 1) * 1000 + 114 = 4114
+*/
+            if (count % 2 == 1) {
+                final int newDigit = lastDigit + digit;
+                newNum = newDigit * decimalShift + newNum;
+                decimalShift *= 10 * ((newDigit / 10 == 1) ? 10 : 1);
+            }
+
+            count++;
+            lastDigit = digit;
+        }
+
+        return isPalindromeDescendant(newNum);
+    }
+}
---
+++ src/test/java/edu/hw1/Task5Test.java
@@ -0,0 +1,70 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task5Test {
+    @Test
+    @DisplayName("Тест с сайта 0")
+    void test0() {
+        boolean bool = Task5.isPalindromeDescendant(123312);
+        assertThat(bool).isTrue();
+    }
+
+    @Test
+    @DisplayName("Тест с сайта 1")
+    void test1() {
+        boolean bool = Task5.isPalindromeDescendant(11211230);
+        assertThat(bool).isTrue();
+    }
+
+    @Test
+    @DisplayName("Тест с сайта 2")
+    void test2() {
+        boolean bool = Task5.isPalindromeDescendant(13001120);
+        assertThat(bool).isTrue();
+    }
+
+    @Test
+    @DisplayName("Тест с сайта 3")
+    void test3() {
+        boolean bool = Task5.isPalindromeDescendant(23336014);
+        assertThat(bool).isTrue();
+    }
+
+    @Test
+    @DisplayName("Тест с сайта 4")
+    void test4() {
+        boolean bool = Task5.isPalindromeDescendant(11);
+        assertThat(bool).isTrue();
+    }
+
+    @Test
+    @DisplayName("Число 9910")
+    void number9910() {
+        boolean bool = Task5.isPalindromeDescendant(9910);
+        assertThat(bool).isTrue();
+    }
+    @Test
+    @DisplayName("Число 1377")
+    void number1377() {
+        boolean bool = Task5.isPalindromeDescendant(1377);
+        assertThat(bool).isTrue();
+    }
+
+    @Test
+    @DisplayName("Число 130177")
+    void number130177() {
+        boolean bool = Task5.isPalindromeDescendant(130177);
+        assertThat(bool).isTrue();
+    }
+
+    @Test
+    @DisplayName("Число 93287")
+    void number93287() {
+        boolean bool = Task5.isPalindromeDescendant(93287);
+        assertThat(bool).isFalse();
+    }
+}
