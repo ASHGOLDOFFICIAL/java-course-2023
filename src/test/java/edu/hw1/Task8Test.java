6b7589c4767cce813473aa67e0572313697d4e1f
Andrey Shaat <links.goldwater@gmail.com>
2023-10-14 01:05:09 +0500

    Added solution for task 8 from homework 1

---
+++ src/test/java/edu/hw1/Task8Test.java
@@ -0,0 +1,141 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public final class Task8Test {
+    @Test
+    @DisplayName("Тест из примера 1")
+    void test1() {
+        final int[][] board = {
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 1, 0, 1, 0},
+                {0, 1, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 1},
+                {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isTrue();
+    }
+
+    @Test
+    @DisplayName("Тест из примера 2")
+    void test2() {
+        final int[][] board = {
+                {1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1},
+                {0, 0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 1, 0, 0, 1, 0, 1},
+                {1, 0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1, 0, 1},
+                {1, 0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isFalse();
+    }
+
+    @Test
+    @DisplayName("Тест из примера 3")
+    void test3() {
+        final int[][] board = {
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isFalse();
+    }
+
+//    Проверяем, чтобы программа не вылетала при коне в углу
+    @Test
+    @DisplayName("Угол: левый верхний")
+    void topLeftCorner() {
+        final int[][] board = {
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isFalse();
+    }
+    @Test
+    @DisplayName("Угол: правый верхний")
+    void topRightCorner() {
+        final int[][] board = {
+                {0, 0, 0, 0, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isFalse();
+    }
+    @Test
+    @DisplayName("Угол: левый нижний")
+    void bottomLeftCorner() {
+        final int[][] board = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isTrue();
+    }
+    @Test
+    @DisplayName("Угол: правый нижний")
+    void bottomRightCorner() {
+        final int[][] board = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isTrue();
+    }
+    @Test
+    @DisplayName("Все углы")
+    void allCorners() {
+        final int[][] board = {
+                {1, 0, 0, 0, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 1}
+        };
+        final boolean noCaptures = Task8.knightBoardCapture(board);
+        assertThat(noCaptures).isTrue();
+    }
+}
