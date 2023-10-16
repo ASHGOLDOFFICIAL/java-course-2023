af7fe4244e6fefd8f006f5f10e98d1928f10cd39
Andrey Shaat <links.goldwater@gmail.com>
2023-10-16 16:55:44 +0500

    Removed newline

--- src/main/java/edu/Task.java
+++ src/main/java/edu/Task.java
@@ -7,6 +7,5 @@ public class Task {
     protected static final Logger LOGGER = LogManager.getLogger();
     protected static final int DECIMAL_BASE = 10;
 
-    protected Task() {
-    }
+    protected Task() {}
 }
